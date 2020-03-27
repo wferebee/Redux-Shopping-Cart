93d3d8a5c3d51f05433c12ef6acbf94df99ce897
DP3PO <williampferebee@gmail.com>
2020-03-26 20:15:23 -0400

    Updates

---
+++ precache-manifest.568df1981088f2668878e8d64e46a756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce1ca53cb7623bdcd2a1614d2bd5607",
+    "url": "/my-app-advance/index.html"
+  },
+  {
+    "revision": "80e0a4186d0e8e60acad",
+    "url": "/my-app-advance/static/css/main.27698f69.chunk.css"
+  },
+  {
+    "revision": "c4e698fa6a1c7f85703b",
+    "url": "/my-app-advance/static/js/2.e342ab83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app-advance/static/js/2.e342ab83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e0a4186d0e8e60acad",
+    "url": "/my-app-advance/static/js/main.43973945.chunk.js"
+  },
+  {
+    "revision": "ccd1af169c0292880443",
+    "url": "/my-app-advance/static/js/runtime-main.bc1d07f7.js"
+  }
+]);
